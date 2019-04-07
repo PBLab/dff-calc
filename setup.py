c0c56a8bd32c88c0bff5d6d6e9b68da952326d9a
HagaiHargil <hagaihargil@gmail.com>
2019-04-07 10:17:56 +0300

    FEATURE: added py37 support

--- setup.py
+++ setup.py
@@ -20,5 +20,6 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )

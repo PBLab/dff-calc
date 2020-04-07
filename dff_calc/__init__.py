e4639d40736338fda77e8e8c64308bd7d0fac811
HagaiHargil <hagaihargil@gmail.com>
2020-04-07 10:14:26 +0300

    function-based interface

--- dff_calc/__init__.py
+++ dff_calc/__init__.py
@@ -1,5 +1,7 @@
 """dff_calc - Simple dF/F calculation for neural calcium traces"""
 
-__version__ = '0.1.5'
+from dff_calc.df_f_calculation import dff_calc
+
+__version__ = '0.2'
 __author__ = 'Hagai Har-Gil <hagaihargil@protonmail.com>'
-__all__ = []
+__all__ = ['dff_calc']
--- dff_calc/df_f_calculation.py
+++ dff_calc/df_f_calculation.py
@@ -1,92 +1,94 @@
 import numpy as np
-import attr
-from attr.validators import instance_of
 import pandas as pd
 
 
-@attr.s
-class DffCalculator:
-    """
-    Takes a matrix with rows as independent fluorescent traces,
+def dff_calc(
+    data: np.ndarray,
+    fps: float = 30.0,
+    tau_0: float = 0.1,
+    tau_1: float = 0.35,
+    tau_2: float = 2.0,
+    invert: bool = False,
+):
+    """Calculates dF/F from calcium traces, based on
+    https://www.nature.com/articles/nprot.2010.169.
+
+    Takes a matrix with rows as independent fluorescent traces
     and returns a similar-sized matrix with the corresponding dF/F values
-    calculated based on https://www.nature.com/articles/nprot.2010.169
+    calculated based on https://www.nature.com/articles/nprot.2010.169.
     The starting frames might have a 0 dF/F value, due to NaNs being
     converted to 0. This happens following the running window operations
     that are preformed on the data.
-    Parameters:
-    -----------
-        data [np.ndarray]: (cell x time)
-        fps [float]: frame rate (Hz)
-        tau_0 [float]: exponential smoothing factor in seconds
-        tau_1 [float]: F0 smoothing parameter in seconds
-        tau_2 [float]: time window before t to minimize
-        invert [bool]: False (default) if the transient is expected to be positive, True otherwise
+
+    Parameters
+    ----------
+    data : np.ndarray
+        dF/F traces with dimensions (cell x time)
+    fps : float, optional
+        Frame rate (Hz)
+    tau_0 : float, optional
+        Exponential smoothing factor in seconds
+    tau_1 : float, optional
+        F0 smoothing parameter in seconds
+    tau_2 : float, optional
+        Time window before each measurement to minimize
+    invert : bool, optional
+        False (default) if the transient is expected to be positive, True
+        otherwise.
+
+    Returns
+    -------
+    dff : np.ndarray
+        A 2D array, each row being a calculated dF/F trace.
     """
+    tau_0, tau_1, tau_2, min_per = _apply_units_and_corrections(
+        fps, tau_0, tau_1, tau_2
+    )
+    if invert:
+        data = -data
 
-    data = attr.ib(validator=instance_of(np.ndarray))
-    fps = attr.ib(default=30.0, validator=instance_of(float))  # Hz
-    tau_0 = attr.ib(
-        default=0.1, validator=instance_of(float)
-    )  # seconds, EWMA parameter
-    tau_1 = attr.ib(
-        default=0.35, validator=instance_of(float)
-    )  # seconds, smoothed F0 parameter
-    tau_2 = attr.ib(
-        default=2.0, validator=instance_of(float)
-    )  # seconds, time window before t to minimize
-    invert = attr.ib(default=False, validator=instance_of(bool))
-    f0 = attr.ib(init=False)
-    unfiltered_dff = attr.ib(init=False)
-    dff = attr.ib(init=False)
-    trace_num = attr.ib(init=False)  # number of traces
-    sample_num = attr.ib(init=False)  # number of samples per trace
-    min_per = attr.ib(init=False)
+    f0 = _calc_f0(data, tau_1, tau_2, min_per)
+    unfiltered_dff = _calc_dff_unfiltered(f0, data)
+    dff = _filter_dff(unfiltered_dff, tau_0, min_per)
+    if data.ndim == 1:
+        return np.atleast_2d(dff.to_numpy().ravel())
+    else:
+        return dff.to_numpy().T
 
-    def calc(self) -> np.ndarray:
-        """ Main function to run the calculation
-        :returns: Filtered dF/F matrix
-        """
-        self._calc_f0()
-        self._calc_dff_unfiltered()
-        self._filter_dff()
-        if self.data.ndim == 1:
-            return self.dff.to_numpy().ravel()
-        else:
-            return self.dff.to_numpy().T
 
-    def __attrs_post_init__(self):
-        """ Change params to have fitting units """
-        self.tau_0 = self.fps * self.tau_0
-        self.tau_1 = int(self.fps * self.tau_1)
-        self.tau_2 = int(self.fps * self.tau_2)
-        self.trace_num = self.data.shape[0]
-        self.min_per = max(1, int(self.fps / 10))
-        if self.invert:
-            self.data = -self.data
+def _apply_units_and_corrections(fps, tau_0, tau_1, tau_2):
+    """Correct the given parameters based on the FPS"""
+    tau_0 = fps * tau_0
+    tau_1 = int(fps * tau_1)
+    tau_2 = int(fps * tau_2)
+    min_per = max(1, int(fps / 10))
+    return tau_0, tau_1, tau_2, min_per
 
-    def _calc_f0(self):
-        """
-        Create the F_0(t) baseline for the dF/F calculation using a boxcar window
-        """
-        data = pd.DataFrame(self.data.T)
-        self.f0 = (
-            data.rolling(window=self.tau_1, win_type="boxcar")
-            .mean()
-            .rolling(window=self.tau_2, min_periods=self.min_per)
-            .min()
-            + np.finfo(float).eps
-        )
 
-    def _calc_dff_unfiltered(self):
-        """ Subtract baseline from current fluorescence """
-        f0 = self.f0.to_numpy()
-        raw_calc = (self.data.T - f0) / f0
-        self.unfiltered_dff = pd.DataFrame(raw_calc).fillna(0)
+def _calc_f0(
+    data: np.ndarray, tau_1: float, tau_2: float, min_per: int
+) -> pd.DataFrame:
+    """Create the F_0(t) baseline for the dF/F calculation using a boxcar window."""
+    data = pd.DataFrame(data.T)
+    f0 = (
+        data.rolling(window=tau_1, win_type="boxcar")
+        .mean()
+        .rolling(window=tau_2, min_periods=min_per)
+        .min()
+        + np.finfo(float).eps
+    )
+    return f0
 
-    def _filter_dff(self):
-        """ Apply an exponentially weighted moving average to the dF/F data """
-        self.dff = (
-            self.unfiltered_dff.ewm(halflife=self.tau_0, min_periods=self.min_per)
-            .mean()
-            .fillna(0)
-        )
+
+def _calc_dff_unfiltered(f0: pd.DataFrame, data: np.ndarray):
+    """ Subtract baseline from current fluorescence """
+    f0 = f0.to_numpy()
+    raw_calc = (data.T - f0) / f0
+    unfiltered_dff = pd.DataFrame(raw_calc).fillna(0)
+    return unfiltered_dff
+
+
+def _filter_dff(unfiltered_dff: pd.DataFrame, tau_0: float, min_per: int):
+    """Apply an exponentially weighted moving average to the dF/F data. """
+    dff = unfiltered_dff.ewm(halflife=tau_0, min_periods=min_per).mean().fillna(0)
+    return dff
